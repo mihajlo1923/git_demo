0d8040275599b597b8fdbbc065cbc4f7b0f2ba18
mihajlo1923 <pusonjamihajlo@gmail.com>
2023-12-10 10:10:10 +0100

    sredjeno

---
+++ objekti.js
@@ -0,0 +1,5 @@
+let reviews = [
+    { text: "Odličan proizvod!", rating: 5, user: "Mihajlo" },
+    { text: "Proizvod je u redu.", rating: 3, user: "Luka" },
+    { text: "Loš proizvod.", rating: 1, user: "Jovan" }
+  ];
--- recenzije.js
+++ recenzije.js
@@ -1,8 +1,4 @@
-let reviews = [
-    { text: "Odličan proizvod!", rating: 5, user: "Mihajlo" },
-    { text: "Proizvod je u redu.", rating: 3, user: "Luka" },
-    { text: "Loš proizvod.", rating: 1, user: "Jovan" }
-  ];
+
 
 
   function displayReviews() {
